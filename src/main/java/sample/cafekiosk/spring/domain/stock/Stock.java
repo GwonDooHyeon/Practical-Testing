d2611843759a577d644e875fed3ffaae278525b6
GwonDooHyeon <secure3141@gmail.com>
2024-07-15 23:17:36 +0900

    비즈니스 레이어 테스트 완료

--- src/main/java/sample/cafekiosk/spring/domain/stock/Stock.java
+++ src/main/java/sample/cafekiosk/spring/domain/stock/Stock.java
@@ -24,10 +24,27 @@ public class Stock extends BaseEntity {
     private int quantity;
 
     @Builder
-    private Stock(Long id, String productNumber, int quantity) {
-        this.id = id;
+    private Stock(String productNumber, int quantity) {
         this.productNumber = productNumber;
         this.quantity = quantity;
     }
 
+    public static Stock create(String productNumber, int quantity) {
+        return Stock.builder()
+                .productNumber(productNumber)
+                .quantity(quantity)
+                .build();
+    }
+
+    public boolean isQuantityLessThan(int quantity) {
+        return this.quantity < quantity;
+
+    }
+
+    public void deductQuantity(int quantity) {
+        if (isQuantityLessThan(quantity)) {
+            throw new IllegalArgumentException("차감할 재고 수량이 없습니다.");
+        }
+        this.quantity -= quantity;
+    }
 }
---
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -0,0 +1,39 @@
+package sample.cafekiosk.spring.domain.product;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class ProductTypeTest {
+
+    @DisplayName("상품 타입이 재고 관련 타입이 아닌 경우를 체크한다.")
+    @Test
+    void containsStockType() throws Exception {
+        // given
+        ProductType givenType = ProductType.HANDMADE;
+
+        // when
+        boolean result = ProductType.containsStockType(givenType);
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @DisplayName("상품 타입이 재고 관련 타입인 경우를 체크한다.")
+    @Test
+    void containsStockType2() throws Exception {
+        // given
+        ProductType givenType = ProductType.BAKERY;
+
+        // when
+        boolean result = ProductType.containsStockType(givenType);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+
+}
